540f078a01be1d65f9717fe956f5001de690cfe7
benjamincristian <112953853+benjamincristian@users.noreply.github.com>
2023-03-28 17:42:44 +0200

    V14 / Now search bar works

--- post/models.py
+++ post/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post_id = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE, null=True, blank=True)
+    post_id = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True, null=True)
