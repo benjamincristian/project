c4144e3db3d2c4ae8dfdb0c31fe803a9bfff9abf
benjamincristian <112953853+benjamincristian@users.noreply.github.com>
2023-03-28 23:17:25 +0200

    V18 / Improvements made to the HTML pages

--- post/admin.py
+++ post/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
-
-from django.contrib import admin
-
 from post.models import Post, Comment
 
 admin.site.register(Post)
 admin.site.register(Comment)
-
-
-
